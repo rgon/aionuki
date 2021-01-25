613354bf2267e08b6fe70e5804cf409c528a681c
rgon <rgon@outlook.com>
2021-01-25 17:49:33 +0100

    Packaging.

--- setup.py
+++ setup.py
@@ -11,5 +11,13 @@ setup(
     author="Gonzalo Ruiz",
     url="https://github.com/rgon/aionuki",
     packages=find_packages(),
-    install_requires=["requests"],
+    install_requires=["aiohttp"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",  # "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+        "Intended Audience :: Developers",
+        "Topic :: Home Automation",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.7",
+    ],
 )
